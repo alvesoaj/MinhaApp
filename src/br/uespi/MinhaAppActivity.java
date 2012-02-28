589b62cb7b02f7505d527d9c840dafd92e3b9066
AJ Alves <zerokol@ymail.com>
2012-02-28 08:39:13 -0300

    p

--- src/br/uespi/MinhaAppActivity.java
+++ src/br/uespi/MinhaAppActivity.java
@@ -2,12 +2,17 @@ package br.uespi;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MinhaAppActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		TextView myTextView = (TextView) findViewById(R.id.myTextView);
+		
+		myTextView.setText("AJ Alves");
+	}
 }
